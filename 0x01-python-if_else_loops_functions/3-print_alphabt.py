66f40f242d401b6f83760a764b6c16e6114a7559
Nathan <sisay954@gmail.com>
2023-03-07 11:47:45 -0800

    alpha exclude

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for letter in range(97,123):
-    if chr(letter) not in ['e','p']:
+for letter in range(97, 123):
+    if chr(letter) not in ['e', 'q']:
         print("{}".format(chr(letter)), end="")
