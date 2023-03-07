5c0a2d30fca4622f4bdad503b7e82e31cb936cc5
Nathan <sisay954@gmail.com>
2023-03-07 01:07:49 -0800

    concat

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str.split(',')[2].split()[0] + ' ' + str.split(',')[2].split()[1] + ' ' + str.split()[-4] + ' ' + str.split()[0])
-print("\n")
+ str = str[39:67] + str[107:112] + str[:6]
+print(str)
