9630b60db12248490ef4b1b5c17001007d746b35
Nathan <sisay954@gmail.com>
2023-03-09 11:56:25 -0800

    argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    import sys
+
+    counter = len(sys.argv) - 1
+    if counter == 0:
+        print("0 arguments")
+    elif counter == 1:
+        print("1 argument")
+    else:
+        print("{} arguments:".format(counter))
+    for i in range(counter):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
