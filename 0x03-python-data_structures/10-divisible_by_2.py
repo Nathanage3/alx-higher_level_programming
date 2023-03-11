60bf916c43a5237ad2e373c70615aaacaf840e27
Nathan <sisay954@gmail.com>
2023-03-11 02:42:32 -0800

    task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,6 +3,6 @@ def divisible_by_2(my_list=[]):
     result = []
     for num in my_list:
         if num % 2 == 0:
-            result.append(True)
+    result.append(True)
         else:
-            result.append(False)
+    result.append(False)
