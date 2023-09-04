ca183e345d8fdc32c0208d6b5915d7996a816c20
nathanage3 <sisay954@gmail.com>
2023-09-04 14:48:00 -0700

    task 6

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print(f"Welcome to {str1} {str2}!")
+str1 += " " + str2
+print(f"Welcome to {str1}!")
