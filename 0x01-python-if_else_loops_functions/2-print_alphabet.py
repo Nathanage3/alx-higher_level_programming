00c4816ceef1c3fcf77391b5d7097819ad3a1753
nathanage3 <sisay954@gmail.com>
2023-09-05 14:15:19 -0700

    task 1-3

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    print("{}".format(chr(i)), end="");
+for c in range(97, 123):
+    print("{}".format(chr(c)), end="")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
-#!/bin/python3
-for i in range(97, 123):
-    if i == 101 or i == 113:
+#!/usr/bin/python3
+for c in range(97, 123):
+    if c == 101 or c == 113:
         continue
-    print("{}".format(chr(i)), end="");
+    print("{}".format(chr(c)), end="")
