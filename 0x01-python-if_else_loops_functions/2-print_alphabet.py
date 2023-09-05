1474e67ae831bb185e33688c974e676a596529e6
nathanage3 <sisay954@gmail.com>
2023-09-05 14:04:31 -0700

    edited

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
-#!/bin/python3
-for i in range(97, 122):
-    print("{:c}".format(i), end="");
+#!/usr/bin/python3
+for i in range(97, 123):
+    print("{}".format(chr(i)), end="");
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/bin/python3
-for i in range(97, 122):
+for i in range(97, 123):
     if i == 101 or i == 113:
         continue
-    print("{:c}".format(i), end="");
+    print("{}".format(chr(i)), end="");
