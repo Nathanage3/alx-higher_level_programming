42f7cf7f1f86dd7936074e601f50ba3ca49af5d0
nathanage3 <sisay954@gmail.com>
2023-09-10 06:52:54 -0700

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,5 +3,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
     result = ()
     a = tuple_a + (0, 0)
     b = tuple_b + (0, 0)
-    result = tuple(a[0] + b[0], a[1] + b[1])
+    result = a[0] + b[0], a[1] + b[1]
     return (result)
