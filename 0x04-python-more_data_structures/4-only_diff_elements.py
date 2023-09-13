49167fea7e98745087d849eca5176af76ffe21a9
nathanage3 <sisay954@gmail.com>
2023-09-13 09:30:41 -0700

    task 4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    odd_set = set_1.symmetric_difference(set_2)
-    return odd_set
+    diff_set = set_1.symmetric_difference(set_2)
+    return diff_set
