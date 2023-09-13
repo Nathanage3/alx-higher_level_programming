e7c089f4cf7a072a4d14ca6967248eb2315f171b
nathanage3 <sisay954@gmail.com>
2023-09-13 09:07:31 -0700

    task 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    for i in set_1:
-        for j in set_2:
-            if i == j:
-                return i
+    common_set = set_1.intersection(set_2)
+    return common_set
