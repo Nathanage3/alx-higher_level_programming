af9b5f1f85bc5fdbbd1c14e0d67d723ffe87a3a8
nathanage3 <sisay954@gmail.com>
2023-10-02 14:13:24 -0700

    task 5

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""Define a rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a rectangle.
+
+        Args:
+        width (int): the width of rectangle.
+        height (int): the height of a rectangle.
+
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get/set the width of a rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get/set a height of a rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Get the area."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Get the perimeter."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Initialize a string."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        line = []
+        for i in range(self.__height):
+            line.append("#" * self.__width)
+        return "\n".join(line)
+
+    def __repr__(self):
+        """Return a formal representation of the rectangle."""
+        return f"Rectangle({self.width}, {self.height})"
+
+    def __del__(self):
+        """Print a message when an instance is deleted."""
+        print("Bye rectangle...")
