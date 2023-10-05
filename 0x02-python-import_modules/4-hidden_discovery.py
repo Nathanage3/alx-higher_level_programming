f20a86d5e0aae6de8f6fcaee5d05a36e98e6467d
nathanage3 <sisay954@gmail.com>
2023-10-05 15:04:22 -0700

    updated task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -17,8 +17,3 @@ def print_names_from_module(filename):
         # Print names in sorted order
         for name in sorted(valid_names):
             print(name)
-
-
-if __name__ == '__main__':
-    # Provide path to the compiled module as argument
-    print_names_from_module('hidden_4.pyc')
