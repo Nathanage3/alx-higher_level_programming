0bc7cebccf4cfb0806ce1aaa23693eab62baf043
nathanage3 <sisay954@gmail.com>
2023-10-10 14:33:52 -0700

    read a file in python

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,7 +2,11 @@
 """This is a module for opening file."""
 
 def read_file(filename=""):
-    """Read the contents of the given file and print them to stdout."""
+    """Read the contents of the given file and print them to stdout.
+    Args:
+        filename(str): the filename
+    """
+
     with open(filename, 'r', encoding='utf-8') as f:
         for line in f:
             print(line, end="");
