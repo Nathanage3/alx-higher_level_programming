9c28cf7e43220756b8185b599afbaa4bdc75b5eb
nathanage3 <sisay954@gmail.com>
2023-10-10 16:37:42 -0700

    add item

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+
+from os import path
+
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
+
+filename = "add_item.json"
+
+if path.exists(filename):
+    items = load_from_json_file(filename)
+else:
+    items = []
+
+items.extend(sys.argv[1:])
+
+save_to_json_file(items, filename)
