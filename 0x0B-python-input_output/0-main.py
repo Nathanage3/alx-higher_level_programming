d7ee1ffdf2e9044ab242d8eb392e8508518daba9
nathanage3 <sisay954@gmail.com>
2023-10-10 14:22:37 -0700

    all files

---
+++ 0x0B-python-input_output/0-main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+read_file = __import__('0-read_file').read_file
+
+read_file("my_file_0.txt")
+
+guillaume@ubuntu:~/0x0B$ cat my_file_0.txt
+We offer a truly innovative approach to education:
+focus on building reliable applications and scalable systems, take on real-world challenges, collaborate with your peers. 
+
+A school every software engineer would have dreamt of!
+guillaume@ubuntu:~/0x0B$ ./0-main.py
+We offer a truly innovative approach to education:
+focus on building reliable applications and scalable systems, take on real-world challenges, collaborate with your peers. 
+
+A school every software engineer would have dreamt of!
---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This is a module for opening file."""
+
+def read_file(filename=""):
+    """Read the contents of the given file and print them to stdout."""
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f:
+            print(line, end="");
