3936046e3e24fef445213f2d7ced82b875ca4aa0
nathanage3 <sisay954@gmail.com>
2023-10-11 12:46:59 -0700

    Avdanced task for inverting a value

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This is a module for inverting an incoming 
+    attribute"""
+
+
+class MyInt(int):
+    """Define a class MyInt"""
+    pass
+
+
+    def __int__(self):
+        """Initializing the class MyIint"""
+        self.num = num
+
+
+    def val_retun(self):
+        """Returning an inverted value"""
+        num = int.inverted
+        return  num
