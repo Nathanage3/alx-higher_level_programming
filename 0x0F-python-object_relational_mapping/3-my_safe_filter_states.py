4a125612092285ec73390d28b38eb9026eb01303
Nathanage3 <sisay954@gmail.com>
2023-12-17 16:19:47 +0300

    task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A script that takes in arguments and display all values from db.
+But this time mysql injection comes to work.  
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(username=argv[1],
+                         passwd=argv[2], db=argv[3])
+    state_name = argv[4]
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY states.id ASC", (state_name,))
+    states = cur.fetchall()
+    for r in states:
+        print(r)
+    cur.close()
+    db.close()
