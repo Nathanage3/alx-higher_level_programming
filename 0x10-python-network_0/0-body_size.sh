bedc1cbb3c3b37bc9bfeda2582e6c0d6e1c9e222
Nathanage3 <sisay954@gmail.com>
2024-01-13 15:11:51 -0800

    if [ -z '' ]

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 #Write a Bash script that takes in a URL, sends a request
-#to that URL, and displays the size of the body of the response
-curl -s "$1" | wc -c
+#to that URL, and displays the size of the body of the
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+url=$1
+
+# Use curl to send a request and display the size of the response body in bytes
+response_size=$(curl -sI "$url" | awk '/Content-Length/ {print $2}' | tr -d '\r\n')
+
+if [ -z "$response_size" ]; then
+    echo "Failed to retrieve response size."
+else
+    echo "Size of the response body for $url is $response_size bytes."
+fi
