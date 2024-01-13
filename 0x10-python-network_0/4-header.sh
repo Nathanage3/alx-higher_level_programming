734b25da523ec642086d2d062ce67d01d791ecca
Nathanage3 <sisay954@gmail.com>
2024-01-13 15:19:21 -0800

    curl -sH <set header>

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#Write a Bash script that takes in a URL as an argument,
-#sends a GET request to the URL, and displays the body of the response
+#Write a Bash script that takes in a URL as an argument
 curl -sH "X-School-User-Id: 98" "${1}"
