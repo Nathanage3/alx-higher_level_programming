a80f25e55d541447ac5695467a93986913b5019b
Nathanage3 <sisay954@gmail.com>
2024-01-13 15:15:44 -0800

    if [ -z '' ]

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-#Write a Bash script that takes in a URL
-#sends a GET request to the URL, and
-#displays the body of the response
+#Write a Bash script that takes URL request
 curl -sL "$1"
