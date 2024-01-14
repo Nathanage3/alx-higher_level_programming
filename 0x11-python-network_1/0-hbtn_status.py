ef9971cd19f5b27047c314175166126979eb7f64
Nathanage3 <sisay954@gmail.com>
2024-01-14 19:42:28 +0300

    urllib.request.urlopen('website-here')

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
 """
-Python script that fetches 'https://alx-intranet.hbtn.io/status'
-You must use the package urllib
-"""
+
+
 if __name__ == '__main__':
-  import urllib.request
-  
-  with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as req:
-    content = req.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(content)))
-    print("\t- content: {}".format(content))
-    print("\t- utf8 content: {}".format(content.decode('utf-8')))
-    
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
