af45fc3ab466e516d9380704c64689dc9d74a798
Nathanage3 <sisay954@gmail.com>
2024-01-14 21:21:21 +0300

    import requests

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+requests model
+"""
+
+if __name__ == '__main__':
+    import requests
+    html = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(html.text.__class__))
+    print("\t- content: {}".format(html.text))
