71357fe7d99c05cb4d24e30d90ad19116b85d6f0
Nathanage3 <sisay954@gmail.com>
2024-02-04 23:47:05 +0300

    movieId 18

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,29 +1,14 @@
 #!/usr/bin/node
 const request = require("request");
-
-const apiUrl = process.argv[2];
-
-if (!apiUrl) {
-  console.error("Usage: node 4-starwars_count.js <api_url>");
-  process.exit(1);
-}
-
-const characterId = 18;
-
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    try {
-      const filmsData = JSON.parse(body).results;
-      const moviesWithWedge = filmsData.filter((film) =>
-        film.characters.includes(
-          `https://swapi-api.alx-tools.com/api/people/${characterId}/`
-        )
-      );
-      console.log(moviesWithWedge.length);
-    } catch (parseError) {
-      console.error("Error parsing response:", parseError);
-    }
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(
+      results.reduce((count, movie) => {
+        return movie.characters.find((character) => character.endsWith("/18/"))
+          ? count + 1
+          : count;
+      }, 0)
+    );
   }
 });
