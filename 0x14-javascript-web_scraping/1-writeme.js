651f26552a8be7d24cd997bddaf891da99898da3
Nathanage3 <sisay954@gmail.com>
2024-02-04 23:10:50 +0300

    creating file and add content to it

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,18 +1,5 @@
 #!/usr/bin/node
 const fs = require("fs");
-
-const filePath = process.argv[2];
-const contentToWrite = process.argv[3];
-
-if (!filePath || !contentToWrite) {
-  console.error("Usage: node 1-writeme.js <file_path> <content_to_write>");
-  process.exit(1);
-}
-
-fs.writeFile(filePath, contentToWrite, "utf8", (error) => {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log(`Content written to ${filePath}`);
-  }
+fs.writeFile(process.argv[2], process.argv[3], (error) => {
+  if (error) console.log(error);
 });
